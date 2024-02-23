cc21d6fef5625b23d3d60b1bd7ece8ba01dcd6ba
HakimAhmadi1 <133155927+HakimAhmadi1@users.noreply.github.com>
2024-02-23 21:35:54 +0500

    updates

--- luckydatabase/settings.py
+++ luckydatabase/settings.py
@@ -27,7 +27,7 @@ with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # PublicIPs: 3.21.57.117    PrivateIPs: 172.31.17.92
 
 # ALLOWED_HOSTS = ["3.21.57.117","127.0.0.1"]
