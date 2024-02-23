8fe14864b52b6248eb672c8bb5c11302150b6fab
HakimAhmadi1 <133155927+HakimAhmadi1@users.noreply.github.com>
2024-02-23 22:27:58 +0500

    update

--- luckydatabase/settings.py
+++ luckydatabase/settings.py
@@ -174,3 +174,8 @@ CSRF_COOKIE_SAMESITE = 'Strict'
 SESSION_COOKIE_SAMESITE = 'Strict'
 CSRF_COOKIE_HTTPONLY = False  # False since we will grab it via universal-cookies
 # SESSION_COOKIE_HTTPONLY = True
+
+
+
+import django_on_heroku
+django_on_heroku.settings(locals())
